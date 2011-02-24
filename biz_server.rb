34ccf5aad77752bf14c1f51a2143aff54eec4911
mac <mac@veryhappythings.co.uk>
2011-02-24 17:27:12 +0000

    Use ruby logger

--- biz_server.rb
+++ biz_server.rb
@@ -1,14 +1,18 @@
 require 'rubygems'
+require 'logger'
 require 'redis'
+
+log = Logger.new(STDOUT)
+log.level = Logger::DEBUG
 
 redis = Redis.new
 
 while 1 do
   Dir.glob(File.join('sensors', '*')).each do |sensor|
-    puts "Running #{sensor}"
+    log.info "Running #{sensor}"
     output = %x[#{sensor}]
-    puts "Received output:"
-    puts output
+    log.info "Received output:"
+    log.info output.chomp
     redis.publish sensor, output
   end
   sleep(20)
