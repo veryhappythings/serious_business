70999f2731467200fcfc4f452ea4de59e19d2bf5
mac <mac@veryhappythings.co.uk>
2011-03-04 10:51:53 +0000

    Server now runs the sensors it is told to, then quits. Therefore, it isnt really a server any more. It is now a cron job sort of thing.

---
+++ serious_business.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'logger'
+require 'redis'
+
+log = Logger.new(STDOUT)
+log.level = Logger::DEBUG
+
+sensors = ARGV[0]
+
+redis = Redis.new
+
+Dir.glob(File.join('sensors', sensors, '*')).each do |sensor|
+  log.info "Running #{sensor}"
+  output = %x[#{sensor}]
+  log.info "Received output:"
+  log.info output.chomp
+  redis.publish sensor, output
+  redis.put "backlog/#{sensor}", output
+end
+
