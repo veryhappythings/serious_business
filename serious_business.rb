4fca459f1ff59b88d094117426226a879f768e26
mac <mac@veryhappythings.co.uk>
2011-03-14 15:50:04 +0000

    Support for multiple directories within a sensor directory

--- serious_business.rb
+++ serious_business.rb
@@ -12,12 +12,14 @@ sensors = ARGV[0]
 
 redis = Redis.new
 
-Dir.glob(File.join('sensors', sensors, '*')).each do |sensor|
-  log.info "Running #{sensor}"
-  output = %x[#{sensor}]
-  log.info "Received output:"
-  log.info output.chomp
-  redis.publish "live/#{sensor}", output
-  redis.lpush "backlog/#{sensor}", output
+Dir.glob(File.join('sensors', sensors, '**', '*')).each do |sensor|
+  unless File.directory? sensor
+    log.info "Running #{sensor}"
+    output = %x[#{sensor}]
+    log.info "Received output:"
+    log.info output.chomp
+    redis.publish "live/#{sensor}", output
+    redis.lpush "backlog/#{sensor}", output
+  end
 end
 
