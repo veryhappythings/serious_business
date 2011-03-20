3034196bb6e21efcb017828ab965d8bd2eebfe74
mac <mac@veryhappythings.co.uk>
2011-03-20 11:05:10 +0000

    Adds date and time - graph labels need work

--- serious_business.rb
+++ serious_business.rb
@@ -1,8 +1,11 @@
 #!/usr/bin/env ruby
 
 require 'rubygems'
+require 'date'
 require 'thread'
 require 'logger'
+require 'json'
+
 require 'redis'
 
 log = Logger.new(STDOUT)
@@ -15,13 +18,15 @@ redis = Redis.new
 log.info("Running #{sensors} sensors")
 Dir.glob(File.join('sensors', sensors, '**', '*')).each do |sensor|
   unless File.directory? sensor
+    date = DateTime.now
     log.info "Running #{sensor}"
     output = %x[#{sensor}]
     output = output.to_i rescue output.chomp
     log.info "Received output:"
     log.info output
-    redis.publish "live/#{sensor}", output
-    redis.lpush "backlog/#{sensor}", output
+    result = {:date => date, :output => output}.to_json
+    redis.publish "live/#{sensor}", result
+    redis.lpush "backlog/#{sensor}", result
   end
 end
 
